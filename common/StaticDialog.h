e2e0f2d2697e1cf89712b76f55bbd15ee2183dee
nmlgc <nmlgc@nmlgc.net>
2016-11-04 03:18:57 +0100

    Fix all errors that keep the x64 version from compiling

--- common/StaticDialog.h
+++ common/StaticDialog.h
@@ -99,7 +99,7 @@ public :
 
 protected :
 	RECT _rc;
-	static BOOL CALLBACK dlgProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+	static INT_PTR CALLBACK dlgProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 	virtual BOOL CALLBACK run_dlgProc(UINT message, WPARAM wParam, LPARAM lParam) = 0;
 
     void alignWith(HWND handle, HWND handle2Align, PosAlign pos, POINT & point);
